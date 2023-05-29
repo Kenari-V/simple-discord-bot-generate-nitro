cd62d1b8eea1e76ac20754666bbec11585dd04dd
Denny Rinaldy <rinaldy.denny991@gmail.com>
2023-05-29 14:31:23 +0700

    discord nitro random generate

---
+++ botanehharam.py
@@ -0,0 +1,41 @@
+import random
+import discord
+from discord.ext import commands
+from discord.commands import Option
+from settingtoken import token
+
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="test"))
+    print('Login {0.user}'.format(bot))
+
+huruf_besar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+huruf_kecil = 'abcdefghijklmnopqrstuvwxyz'
+nomber = '0123456789'
+
+@bot.slash_command(name="generate", description="generate nitro discord")
+async def generatediscord(ctx):
+    besar, kecil, number, = True, True , True
+    all = ""
+
+    if besar:
+        all += huruf_besar
+    if kecil:
+        all += huruf_kecil
+    if number:
+        all += nomber
+
+        length = 16
+        # amount = noyangmauDimasukan
+        
+        # for x in range(int(noyangmauDimasukan)):
+        nitrogen = ''.join(random.sample(all, length))
+
+    await ctx.respond('discord.gift/'+nitrogen)           
+
+
+
+bot.run(token)
